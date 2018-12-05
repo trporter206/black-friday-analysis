a8b505d77eb8474f4ce01bf3084d6b5298f74f76
trporter <torri.porter@yahoo.com>
2018-12-04 23:33:59 -0800

    gender purchasing patterns

--- main.py
+++ main.py
@@ -5,29 +5,52 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 
 bf_data = pd.read_csv("BlackFriday.csv")
+bf_train = bf_data.head(10000)
 
 # print bf_data.isna().any()
 
 
 #clean--------------------------------------------------------------------------
-bf_data.fillna(value=0, inplace=True)
-bf_data['Product_Category_2'] = bf_data['Product_Category_2'].astype(int)
-bf_data['Product_Category_3'] = bf_data['Product_Category_3'].astype(int)
+bf_train.fillna(value=0, inplace=True)
+bf_train['Product_Category_2'] = bf_train['Product_Category_2'].astype(int)
+bf_train['Product_Category_3'] = bf_train['Product_Category_3'].astype(int)
 
-bf_data.drop(columns = ['User_ID', 'Product_ID'], inplace=True)
+bf_train.drop(columns = ['User_ID', 'Product_ID'], inplace=True)
 
 #explore------------------------------------------------------------------------
 def genders():
-    sns.countplot(bf_data['Gender'])
+    # sns.countplot(bf_train['Gender'])
+    sns.boxplot(x= 'Gender', y= 'Purchase', data= bf_train)
 
 def ages():
-    sns.countplot(bf_data['Age'], hue=bf_data['Gender'])
+    sns.countplot(bf_train['Age'], hue=bf_train['Gender'])
+    # sns.boxplot(x='Age', y= 'Purchase', data=bf_train)
 
 def marriage():
-    sns.countplot(bf_data['Age'], hue=bf_data['Marital_Status'])
+    women = bf_train[bf_train.Gender == 'F']
+    sns.countplot(women['Age'], hue=women['Marital_Status'])
+    # sns.boxplot(x='Marital_Status', y= 'Purchase', hue= 'Gender', data=bf_train)
 
 def occupation():
-    sns.countplot(bf_data['Occupation'], hue=bf_data['Marital_Status'])
+    sns.boxplot(x='Occupation', y= 'Purchase', data=bf_train)
 
-occupation()
+def cities():
+    # sns.countplot(bf_train['City_Category'], hue=bf_train['Age'])
+    sns.boxplot(x='City_Category', y= 'Purchase', data=bf_train)
+
+def stay():
+    # sns.countplot(bf_train['Stay_In_Current_City_Years'])
+    sns.boxplot(x='Stay_In_Current_City_Years', y= 'Purchase', data=bf_train)
+
+def purchasers():
+    m0 = pd.Series(bf_train[(bf_train.Marital_Status == 0) & (bf_train.Gender == 'M')]['Purchase'])
+    m1 = pd.Series(bf_train[(bf_train.Marital_Status == 1) & (bf_train.Gender == 'M')]['Purchase'])
+    f0 = pd.Series(bf_train[(bf_train.Marital_Status == 0) & (bf_train.Gender == 'F')]['Purchase'])
+    f1 = pd.Series(bf_train[(bf_train.Marital_Status == 1) & (bf_train.Gender == 'F')]['Purchase'])
+
+    df = pd.DataFrame({'m0': m0, 'm1': m1, 'f0': f0, 'f1': f1})
+    sns.boxplot(data=df)
+
+    
+purchasers()
 plt.show()
